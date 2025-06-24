eb1e1174600843753457055e9f677acdcccded20
rcrosbie <1092cameo@gmail.com>
2025-06-23 21:14:46 -0400

    Refactor AuthorizationHeaderBuilder to use constants and improve parameter building
    filterNot is not needed if null token is not added to the list

--- jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/client/util/AuthorizationHeaderBuilder.kt
+++ jellyfin-api/src/commonMain/kotlin/org/jellyfin/sdk/api/client/util/AuthorizationHeaderBuilder.kt
@@ -2,6 +2,11 @@ package org.jellyfin.sdk.api.client.util
 
 public object AuthorizationHeaderBuilder {
 	public const val AUTHORIZATION_SCHEME: String = "MediaBrowser"
+	private const val KEY_CLIENT = "Client"
+	private const val KEY_VERSION = "Version"
+	private const val KEY_DEVICE_ID = "DeviceId"
+	private const val KEY_DEVICE = "Device"
+	private const val KEY_TOKEN = "Token"
 
 	public fun encodeParameterValue(raw: String): String = raw
 		.trim()
@@ -31,23 +36,22 @@ public object AuthorizationHeaderBuilder {
 		deviceName: String,
 		accessToken: String? = null,
 	): String {
-		val params = arrayOf(
-			"Client" to clientName,
-			"Version" to clientVersion,
-			"DeviceId" to deviceId,
-			"Device" to deviceName,
-			"Token" to accessToken
-		)
+		val params: List<Pair<String, String>> = buildList {
+			add(KEY_CLIENT to clientName)
+			add(KEY_VERSION to clientVersion)
+			add(KEY_DEVICE_ID to deviceId)
+			add(KEY_DEVICE to deviceName)
+			accessToken?.let { token ->
+				add(KEY_TOKEN to token)
+			}
+		}
 
 		// Format: `MediaBrowser key1="value1", key2="value2"`
 		return params
-			// Drop null values (token)
-			.filterNot { (_, value) -> value == null }
-			// Join parts
 			.joinToString(
 				separator = ", ",
 				prefix = "$AUTHORIZATION_SCHEME ",
-				transform = { (key, value) -> buildParameter(key, value!!) }
+				transform = { (key, value) -> buildParameter(key, value) }
 			)
 	}
 }
